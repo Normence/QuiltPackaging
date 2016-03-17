ad304407f3d8cb9c87c9351ae3f66967c7fe44cd
Guojie Luo <gluo@pku.edu.cn>
2016-03-17 19:33:34 +0800

    Finish modification, start debugging

--- partition.cpp
+++ partition.cpp
@@ -1,54 +1,70 @@
 #include <iostream>
 #include <fstream>
-#include "boost/multi_array.hpp"
 #include <string>
+#include <boost/multi_array.hpp>
 
 using namespace std;
 	
-typedef boost::multi_array<double, 5> fd_dbl;
-typedef boost::multi_array<int, 5> fd_int;
-typedef boost::multi_array<int, 4> fod_int;
-typedef fd_dbl::index index_dbl; // not sure functions
-typedef fd_int::index index_int;
-typedef fod_int::index index_int;
+typedef boost::multi_array<double,5> DblArr5d;
+typedef boost::multi_array<int,5> IntArr5d;
+typedef boost::multi_array<int,4> IntArr4d;
 
-extern double compute_base(int x, int y, int w, int h); // (1 + Ac * d / a) ^ (-a)
-extern double compute_comp(); // (1 - f_{wire}) ^ N_{wire}
+/** (1 + Ac * d / alpha) ^ (-alpha) */
+double area_yield(double area, double df_dnst, double alpha) {
+  double yield = pow(1 + area*df_dnst/alpha, -alpha);
+  return yield;
+}
 
+/** (1 - f_wire) ^ count */
+double wire_yield(double f_wire, int count) {
+  return pow(1 - f_wire, count);
+}
 
-
-/**
- * N: resolution of partition
+/** find a recursive slicing partition with an optimal yield
+  @opt    : the optimal yield
+  @pos    : the cut position
+  @w_size : #col of the partition grids
+  @h_size : #row of the partition grids
  */
-int partition(fd_dbl &opt, fd_int &pos, int N, int x_size, int y_size, int w_size, int h_size) {
+int Partition(DblArr5d &opt, IntArr5d &pos, int w_size, int h_size) {
+  int L = 3; // maximum level of partition: an initial guess
+  opt.resize(extents[L+1][w_size][h_size][w_size+1][h_size+1]);
+  pos.resize(extents[L+1][w_size][h_size][w_size+1][h_size+1]);
 
 	int x = 0, y = 0, w = 0, h = 0; // rectangle (x,y,w,h)
-	int l;							// iteration
 	int lx = 0;                     // lx: cut position at x=lx
 	int ly = 0;                     // ly: cut position at y=ly
 	int p = 0;                      // encoding of the cut position
-	double df_dnst, alpha, f_wire;	// defect density, alpha(clustering parameter), failure rate
 
+  double unit;    // a unit length (in meters) of the routing data
+	double df_dnst; // defect density
+  double alpha;   // alpha (clustering parameter)
+  double f_wire;	// failure rate of a cross-die wire
+
+	cout << "Input the unit length (m): ";
+	cin >> unit;
 	cout << "Input the defect density: ";
 	cin >> df_dnst;
-	cout << "Input alpha: ";
+	cout << "Input the clustering parameter: ";
 	cin >> alpha;
 	cout << "Input the failure rate: ";
 	cin >> f_wire;
 
-	for (h = 1; h <= h_size; ++h) {  // traverse every possible rectangle
+  auto meter = [unit](int mark) {return unit*mark}
+
+  // traverse every possible rectangle
+	for (h = 1; h <= h_size; ++h) {
 		for (w = 1; w <= w_size; ++w) {
 			for (y = 0; y <= h_size-h; ++y) {
 				for (x = 0; x <= w_size-w; ++x) {
-					opt[x][y][w][h][0] = compute_base(x, y, w, h); // TO-DO
+          double area = meter(w) * meter(h);
+					opt[0][x][y][w][h] = area_yield(area, df_dnst, alpha);
 				}
 			}
 		}
 	}
 
-	int L = 3; // default initial value
-	l = 1;
-
+  int l = 1; // level of partition
 	while(1){
 		for (; l <= L; ++l) {
 
@@ -57,12 +73,15 @@ int partition(fd_dbl &opt, fd_int &pos, int N, int x_size, int y_size, int w_siz
 					for (y = 0; y <= h_size-h; ++y) {
 						for (x = 0; x <= w_size-w; ++x) {
 
-							opt[x][y][w][h][l] = opt[x][y][w][h][l-1];
-							pos[x][y][w][h][l] = -1; // no partition
+							opt[l][x][y][w][h] = opt[l-1][x][y][w][h];
+							pos[l][x][y][w][h] = -1; // no partition
 
 							for (lx = x+1; lx <= x+w-1; ++lx) {
 								p = lx - (x+1);
-								double yield = compute_comp(); // TO-DO
+								double yield = 
+                    opt[l-1][x][y][lx-x][h] *
+                    opt[l-1][lx][y][w-(lx-x)][h] *
+                    wire_yield(wire_count[1][h][lx][y]);
 
 								if (yield >= opt[x][y][w][h][l]) {
 									opt[x][y][w][h][l] = yield;
@@ -72,7 +91,10 @@ int partition(fd_dbl &opt, fd_int &pos, int N, int x_size, int y_size, int w_siz
 
 							for (ly = y+1; ly <= y+h-1; ++ly) {
 								p = (w-1) + ly - (y+1);  
-								double yield = compute_comp(); // TO-DO
+								double yield =
+                    opt[l-1][x][y][w][ly-y] *
+                    opt[l-1][x][ly][w][h-(ly-y)] *
+                    wire_yield(wire_count[0][w][x][ly]);
 
 								if (yield >= opt[x][y][w][h][l]) {
 									opt[x][y][w][h][l] = yield;
@@ -84,21 +106,22 @@ int partition(fd_dbl &opt, fd_int &pos, int N, int x_size, int y_size, int w_siz
 				}
 			}
 
-			if(pos[x][y][w][h][l] == -1)
+			if (pos[l][0][0][w_size][h_size] == -1)
 				break;
 		}
+    if (pos[l][0][0][w_size][h_size] == -1)
+      break;
 
-		L *= 2;
-		opt.resize(extents[x_size][y_size][w_size+1][h_size+1][L]);
-		pos.resize(extents[x_size][y_size][w_size+1][h_size+1][L]);
+		L += 3; // the next guess of the maximum level of partition
+		opt.resize(extents[L+1][w_size][h_size][w_size+1][h_size+1]);
+		pos.resize(extents[L+1][w_size][h_size][w_size+1][h_size+1]);
 	}
 
 	return l;
-
 }
 
-/** read the input file in the ISPD08 contest format
-  @file_gr : the input file
+/** read the graph file in the ISPD08 contest format
+  @file_gr : the graph file
   @w       : the width of the routing region
   @h       : the height of the routing region
   @x       : the x coordinate of the lower-left corner
@@ -106,7 +129,7 @@ int partition(fd_dbl &opt, fd_int &pos, int N, int x_size, int y_size, int w_siz
   */
 bool ReadGraph(string file_gr, int &w, int &h, int &x, int &y) {
 	ifstream fin(file_gr, ios::in);
-	if (!fin.is_open()) return false;
+	if ( ! fin.is_open()) return false;
 
 	string no_use;
 	fin >> no_use;
@@ -154,7 +177,7 @@ bool is_rlt_prime(int a, int b) {
   return (b == 1);
 }
 
-/** find a proper N' start from the given N */
+/** find a proper N' based on the given N */
 int GetResolution(int w, int h, int N) {
 	while (!is_rlt_prime(w, N) || !is_rlt_prime(h, N))
 		N++;
@@ -162,11 +185,22 @@ int GetResolution(int w, int h, int N) {
 	return N;
 }
 
-bool getData(int argc, char *argv, fod_int &com, int N, const double x_rsl, const double y_rsl, int ini_x, int ini_y)
-{
-	ifstream fin(argv[2], ios::in);
-	if(!fin.is_open())
-		return false;
+/** read the routing file in the ISPD08 contest format
+  @file_rt    : the routing file
+  @wire_count : the number of wires across edges in the partition grids
+  @N          : #row & #col of the partition grids
+  @act_w      : the width of the routing region
+  @act_h      : the height of the routing region
+  @ini_x      : the x coordinate of the lower-left corner
+  @ini_y      : the y coordinate of the lower-left corner
+  */
+bool ReadRouting(string file_rt, IntArr4d &wire_count, int N,
+                 double act_w, double act_h, int ini_x, int ini_y) {
+	ifstream fin(file_rt, ios::in);
+	if ( ! fin.is_open()) return false;
+
+	double rsl_w = act_w / N; // the width of a partition grid
+  double rsl_h = act_h / N; // the height of a partition grid
 
 	int x1, y1, x2, y2;
 	char ch;
@@ -177,33 +211,40 @@ bool getData(int argc, char *argv, fod_int &com, int N, const double x_rsl, cons
 		fin.ignore(255, '\n');
 
 		fin.get(ch);
-		while(ch != '!'){
+		while (ch != '!') {
 			fin >> x1;
 			fin.get();
 			fin >> y1;
-			fin.ignore(5);
+			fin.ignore(5); // assume z in {0,1,...,9}
 			fin >> x2;
 			fin.get();
 			fin >> y2;
 			fin.ignore(255, '\n');
 
-			x1 -= ini_x;
-			y1 -= ini_y;
-			x2 -= ini_x;
-			y2 -= ini_y;
-			x1 = (int)(x1/x_rsl);  //metric transformation (from standard to resolution N)
-			x2 = (int)(x2/x_rsl);
-			y1 = (int)(y1/y_rsl);
-			y2 = (int)(y2/y_rsl);
+      // metric transformation
+      //   before: (x,y) coordinates in the routing region
+      //   after : col and row in the partition grids
+      auto xform = [rsl_w,rsl_h,ini_x,ini_y](int& x, int& y) {
+        x = int((x - ini_x) / rsl_w);
+        y = int((y - ini_y) / rsl_h);
+      }
+      xform(x1, y1);
+      xform(x2, y2);
 
-			if(x1 < x2){
-				for(int i=x1+1; i<=x2; i++)
-					com[1][1][i][y1]++;
+			if (x1 < x2) {
+        assert(y1 == y2);
+        // horizontal wire, vertical edge
+				for (int i=x1+1; i<=x2; i++)
+					wire_count[1][1][i][y1]++;
 			}
-			else if(y1 < y2){
+			else if (y1 < y2) {
+        assert(x1 == x2);
+        // vertical wire, horizontal edge
 				for(int i=y1+1; i<=y2; i++)
-					com[0][1][x1][i]++;
-			}
+					wire_count[0][1][x1][i]++;
+			} else {
+        assert(false);
+      }
 
 			fin.get(ch);
 		}
@@ -214,15 +255,17 @@ bool getData(int argc, char *argv, fod_int &com, int N, const double x_rsl, cons
 
 	fin.close();
 
-	for(int l=2; l<=N; l++) // horizontal
-		for(int x=0; x<=(N-l); x++)
-			for(int y=1; y<N; y++)
-				com[0][l][x][y] = com[0][l-1][x][y] + com[0][1][x+l-1][y];
+	for (int l=2; l<=N; l++) {
+    // horizontal edges
+		for (int x=0; x<=(N-l); x++)
+			for (int y=1; y<=N-1; y++)
+				wire_count[0][l][x][y] = wire_count[0][l-1][x][y] + wire_count[0][1][x+l-1][y];
 
-	for(int l=2; l<=N; l++) // vertical
+    // vertical edges
 		for(int y=0; y<=(N-l); y++)
-			for(int x=1; x<N; x++)
-				com[1][l][x][y] = com[1][l-1][x][y] + com[1][1][x][y+l-1];
+			for(int x=1; x<=N-1; x++)
+				wire_count[1][l][x][y] = wire_count[1][l-1][x][y] + wire_count[1][1][x][y+l-1];
+  }
 
 	return true;
 }
@@ -232,8 +275,8 @@ int main(int argc, char *argv[]) {
     cerr << "Usage: partition <graph> <routing>" << endl;
     return EXIT_FAILURE;
   }
-  string file_gr = argv[1]; // the input file in the ISPD08 contest format
-  string file_rt = argv[2]; // the output file in the ISPD08 contest format
+  string file_gr = argv[1]; // the input (graph) file in the ISPD08 contest format
+  string file_rt = argv[2]; // the output (routing) file in the ISPD08 contest format
 
 	// parser begin //////////////////////////////////////////////////////////////
 	int act_w; // the width of the routing region
@@ -252,34 +295,33 @@ int main(int argc, char *argv[]) {
       << " to " << (N = GetResolution(act_w, act_h, N))
       << endl;
 
-	const double x_rsl = act_w / N; // the width of a partition grid
-  const double y_rsl = act_h / N; // the height of a partition grid
-	int x_size = N;
-  int y_size = N;
-  int w_size = N;
-  int h_size = N;
-
   // the number of wires across an edge in the partition grids
   //   1st dim: 0 - horizontal edge; 1 - vertical edge
-  //   2nd dim: the length of the edge in {1, 2, ..., N-1}
-  //   3rd dim: the x coordinate of the left (bottom) end
-  //   4th dim: the y coordinate of the left (bottom) end
-	fod_int com(boost::extents[2][N][w_size+1][h_size+1]);
-  std::fill(com.data(), com.data() + com.num_elements(), 0);
+  //   2nd dim: the length of the edge in {1, 2, ..., N}
+  //   3rd dim: the x coordinate of the left (bottom) end in {0, 1, ..., N}
+  //   4th dim: the y coordinate of the left (bottom) end in {0, 1, ..., N}
+	IntArr4d wire_count(boost::extents[2][N+1][N+1][N+1]);
+  std::fill(wire_count.data(), wire_count.data() + wire_count.num_elements(), 0);
 	
 	cout << "Reading from the file '" << file_rt << "'..." << endl;
   // count the number of wires across any edge
-	if ( ! getData(argc, argv, com, N, x_rsl, y_rsl, ini_x, ini_y))
+	if ( ! ReadRouting(file_rt, wire_count, N, act_w, act_h, ini_x, ini_y))
 		return -1;
 	cout << "Succeed." << endl;
 	// parser end ////////////////////////////////////////////////////////////////
 
 
-	fd_dbl opt(boost::extents[x_size][y_size][w_size+1][h_size+1][L]); // optimal yield
-	fd_int pos(boost::extents[x_size][y_size][w_size+1][h_size+1][L]); // cut position
+  // optimal yield (opt) and cut position (pos) of a given rectangle
+  //   1st dim: number of levels for partition
+  //   2nd dim: x coordinate of the lower-left corner in {0, 1, ..., N-1}
+  //   3rd dim: y coordinate of the lower-left corner in {0, 1, ..., N-1}
+  //   4th dim: the width of the rectangle in {1, 2, ..., N}
+  //   5th dim: the height of the rectangle in {1, 2, ..., N}
+	DblArr5d opt;
+	IntArr5d pos;
 
-	int l;  // best recursive level
-	l = partition(opt, pos, N, x_size, y_size, w_size, h_size);
+	int l; // the optimal level for partition
+	l = Partition(opt, pos, N, N, N, N);
 	
 	return EXIT_SUCCESS;
 }
